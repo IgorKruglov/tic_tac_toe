6bb3aedac388841537e4b1ef22321c5d2940e7a2
Igor Kruglov <ia.kruglov@jet.su>
2024-04-05 14:25:04 +0300

    final

---
+++ file_actions.py
@@ -0,0 +1,7 @@
+# file_actions.p
+# Открыть на запись файл example.txt
+file = open('example.txt', 'w', encoding='utf-8')
+# Записать в файл строку.
+file.write('Зевну, укроюсь с головою,\nбудильник заведу на март.\n')
+# Закрыть файл.
+file.close()
